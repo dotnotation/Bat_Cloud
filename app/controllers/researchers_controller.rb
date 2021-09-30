1307cdd5d06c1469251d7ca89f7601ff82cd29b1
dotnotation <dotinmotionproductions@gmail.com>
2021-09-30 19:03:13 -0400

    Started working on Sessions and Researchers controllers

--- app/controllers/researchers_controller.rb
+++ app/controllers/researchers_controller.rb
@@ -1,2 +1,45 @@
 class ResearchersController < ApplicationController
+    def index
+        @researchers = Researcher.all
+    end
+
+    def show
+        find_researcher
+    end
+
+    def new
+        @researcher = Researcher.new 
+    end
+
+    def create
+        @researcher = Researcher.new(researcher_params)
+        if @researcher.save
+            flash[:success] = "Your account has been created."
+            session[:researcher_id] = @researcher.id 
+            redirect_to bats_path
+        else
+            flash[:danger] = "#{@researcher.errors.full_messages.join(", ")}"
+            render :new
+        end
+    end
+
+    def edit
+
+    end
+
+    def update
+    end
+    
+    def destroy
+    end
+
+    private
+
+    def researcher_params
+        params.require(:user).permit(:name, :email, :password, :address, :phone_number, :organization_id, :access)
+    end
+
+    def find_researcher
+        @researcher = Researcher.friendly.find(params[:id])
+    end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+
+    def new
+    end
+
+    def create
+        @researcher = Researcher.find_by(email: params[:email])
+        if @researcher && researcher.authenticate(params[:password])
+            session[:researcher_id] = @researcher.id 
+            flash[:success] = "Welcome #{@researcher.name}! You are now logged in."
+            redirect_to bats_path
+        else
+            flash[:danger] = "Login failed"
+            render :new
+        end
+    end
+
+    def delete
+        session.clear
+        redirect_to login_path
+    end
 end
