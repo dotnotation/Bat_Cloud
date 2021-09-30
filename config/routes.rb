133783b84869398def4587e99843d9145745b603
dotnotation <dotinmotionproductions@gmail.com>
2021-09-30 18:44:34 -0400

    Set up Static and Sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   resources :bats
   resources :notes
   resources :researchers
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'static#home'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sesssions#create'
+
+  delete '/logout', to: 'sessions#delete'
 end
