cca15beb76ad247ad8faad47b4c5c4ff11f4a18b
dotnotation <dotinmotionproductions@gmail.com>
2021-10-02 17:55:46 -0400

    Working on researchers show view and displaying buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,12 @@
 module ApplicationHelper
 
     def current_researcher
-        @researcher ||= Researcher.find_by_id(session[:researcher_id])
+        @current_researcher ||= Researcher.find_by_id(session[:researcher_id])
     end
 
     def logged_in?
         !!current_researcher
     end
-
-    # def self.format_date
-    #     self.strftime("%B %d, %Y at %I:%M %P")
-    # end
 
     def admin?
         current_researcher.access == "admin"
